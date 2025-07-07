598956bdddc2bd97bb3c228205ea162c15948b4d
Arthakker <arthuronyeanusi@gmail.com>
2025-07-07 18:20:25 +0100

    Completed authRoute, safe to delete this branch

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,1 +1,12 @@
 import { register, login } from "../controllers/authController";
+import express from "express";
+
+const router = express.Router();
+
+//route for registeration
+router.post("/auth/register", register);
+
+//route to login
+router.post(".auth/login", login);
+
+export default router;
