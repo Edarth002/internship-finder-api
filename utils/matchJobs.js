837c97075639f57c787ef16c87d9dced0b937b19
Arthakker <arthuronyeanusi@gmail.com>
2025-07-21 00:58:50 +0100

    Applied the matchJobs function to ensure applications are only from sites within reach

--- utils/matchJobs.js
+++ utils/matchJobs.js
@@ -1,10 +1,8 @@
-import haversine 
+import haversineFunction from "./haversine";
 
-export async function matchJobs() {
-     
-    try {
-        
-    } catch (error) {
-        
-    }
+export function matchJobs(lat1, lon1, jobs, maxDistance = 50000) {
+  return jobs.filter((job) => {
+    const distance = haversineFunction(lat1, lon1, job.lat, job.lon);
+    return distance <= maxDistance;
+  });
 }
