c5c2b60b344f3a4830c28001f377b5f0ea210615
Arthakker <arthuronyeanusi@gmail.com>
2025-07-21 01:41:35 +0100

    Added nearby jobs feature to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRoute from "./routes/authRoute.js";
 import profileRoute from "./routes/profileRoute.js";
 import applicationRoute from "./routes/applicationRoute.js";
+import nearbyjobsRoute from "./routes/nearbyjobsRoute.js";
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,10 @@ app.use("/api/profile", profileRoute);
 //Route to applications in order of crud(create, read/get, update, delete) operations
 app.use("/api/applications", applicationRoute);
 
+//Route to get nearbyjobs
+app.use("/api/nearbyjobs", nearbyjobsRoute);
+
+//Default/Home Route
 app.get("/", (req, res) => {
   res.send("Internship Finder API is running 🎯");
 });
