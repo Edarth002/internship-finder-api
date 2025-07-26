4eb205e730d5c953c6a4b78be9ba64fdb7383fd8
Arthakker <arthuronyeanusi@gmail.com>
2025-07-26 10:19:22 +0100

    No need to declare maxdistance in matchJobs function so that it is passed dynamically

--- utils/matchJobs.js
+++ utils/matchJobs.js
@@ -1,6 +1,6 @@
 import haversineFunction from "./haversine.js";
 
-export function matchJobs(lat1, lon1, jobs, maxDistance = 50000) {
+export function matchJobs(lat1, lon1, jobs, maxDistance) {
   return jobs.filter((job) => {
     const distance = haversineFunction(lat1, lon1, job.latitude, job.longitude);
     return distance <= maxDistance;
