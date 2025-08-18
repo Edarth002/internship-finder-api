5ad909e0722583a4625e1dfd98d3b39e4e450b18
Arthakker <arthuronyeanusi@gmail.com>
2025-08-19 00:23:25 +0100

    successfully connected to aiven MySQL database

--- prisma/migrations/20250708094041_update_user_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `fullname` VARCHAR(191) NULL,
-    MODIFY `number` VARCHAR(191) NULL,
-    MODIFY `age` INTEGER NULL,
-    MODIFY `department` VARCHAR(191) NULL,
-    MODIFY `school` VARCHAR(191) NULL,
-    MODIFY `industry` VARCHAR(191) NULL;
--- utils/prisma.js
+++ utils/prisma.js
@@ -1,11 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import fs, { writeFileSync } from "fs";
-
-if (process.env.CA_CERTIFICATE) {
-  const caPath = "/temp/ca.pem";
-  writeFileSync(caPath, process.env.CA_CERTIFICATE);
-  process.env.NODE_EXTRA_CA_CERTS = caPath;
-}
 
 const prisma = new PrismaClient();
 
